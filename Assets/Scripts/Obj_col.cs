a81983d2321198e9a459b4e5d9c67ba28f4a1474
Alexandre Auclair <alexandre26auclair@gmail.com>
2020-11-29 11:25:28 -0500

    adding level change and healing items

---
+++ Assets/Scripts/Obj_col.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obj_col : MonoBehaviour
+{
+    private HealthScript healthScript;
+    private GameObject thePlayer;
+
+    void Awake()
+    {
+        thePlayer = GameObject.Find("gladiateur");
+        healthScript = thePlayer.GetComponent<HealthScript>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "gladiateur")
+        {
+            healthScript.health += 30f;
+            Destroy(gameObject);
+        }
+        
+    }
+}
